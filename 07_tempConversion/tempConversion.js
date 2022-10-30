ef35c789f417e648703f3d0e7076dd8a45f9a488
IsraelBlck <Israelonoja21@gmail.com>
2022-10-30 01:58:50 +0100

    Completed exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function(numb) {
+  ////converting from Fahrenheit to Celsius
+  let convertToCelsiusResult = (numb - 32) * 0.5556;
 
+  //// rounding result to the rounded to one decimal place and returning result
+  return (rounded = Math.round(convertToCelsiusResult * 10) / 10);
 };
 
-const ctof = function() {
-
+const ctof = function(numb) {
+  ////converting from Celsius to Fahrenheit
+  let convertToFahrenheitResult = numb * 1.8 + 32;
+  //// rounding result to the rounded to one decimal place and returning result
+  return (rounded = Math.round(convertToFahrenheitResult * 10) / 10);
 };
 
 // Do not edit below this line
