efac77144d51c978c045df837ad5ec1a6fe914be
IsraelBlck <Israelonoja21@gmail.com>
2022-11-08 03:43:07 +0100

    updated and completed my calculator js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (x, y) {
+  let result = x + y;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  let result = x - y;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let x = numbers.reduce((a, b) => (a + b),0);
+  return x;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let result = numbers.reduce((x, y) => y * x, 1);
+    return result
 };
 
-const power = function() {
-	
+const power = function (x,y) {
+  let result = Math.pow(x,y);
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = num;
+  if(num === 0 || num === 1)
+    return 1
+  else if(num === 2)
+    return 2
+  while(num > 1){
+    num--;
+    result *= num
+  }
+  return result
 };
 
 // Do not edit below this line
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
